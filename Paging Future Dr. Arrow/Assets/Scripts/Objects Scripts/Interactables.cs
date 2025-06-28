6113bb9a8f797029b6838682c4d07c50dd8045ad
Frame-solid <102352741+Frame-solid@users.noreply.github.com>
2025-06-27 23:17:05 -0400

    god help us

--- Paging Future Dr. Arrow/Assets/Scripts/Objects Scripts/Interactables.cs
+++ Paging Future Dr. Arrow/Assets/Scripts/Objects Scripts/Interactables.cs
@@ -1,7 +1,9 @@
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 using static System.Net.Mime.MediaTypeNames;
+using static UnityEngine.AdaptivePerformance.Provider.AdaptivePerformanceSubsystemDescriptor;
 using Text = UnityEngine.UI.Text;
 
 
@@ -13,6 +15,14 @@ public class Interactables : MonoBehaviour
     public GameObject interaction_debrief_UI; //ref to Canvas or Text parent
     private Text interaction_text; //ref to the Text element
     private Text interaction_Debrief_Text;//ref to the text element (debrief text/notes on BMT)
+
+    //npc dialogue interactions
+    [SerializeField] private string prompt;
+    [SerializeField] private GameObject uiPanel;
+    [SerializeField] private TextMeshPro promptText;
+    
+    public Conversation convo;
+    public bool isDisplayed = false;
 
 
     //[SerializeField] GameObject miniGame; //can be a physical 2d object or change to open a minigame scene
@@ -31,6 +41,17 @@ public class Interactables : MonoBehaviour
 
         }
         */
+
+        uiPanel.SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDisplayed & this.CompareTag("NPC")) {
+            if (Input.GetMouseButtonDown(0)) {
+                DialogueManager.StartConversation(convo);
+            }
+        }
     }
 
 
@@ -44,10 +65,16 @@ public class Interactables : MonoBehaviour
         if (other.tag == "Player")
         {
             highlight.SetActive(true);
+            SetUp(this.prompt);
 
             if (interaction_debrief_UI != null)
             {
                 interaction_debrief_UI.SetActive(true);//shows the text
+                //interaction_.Text = "Press [E} to read Me!";// set the text message
+
+            }
+
+            if (this.CompareTag("NPC")) {
                 //interaction_.Text = "Press [E} to read Me!";// set the text message
 
             }
@@ -59,6 +86,7 @@ public class Interactables : MonoBehaviour
         if (other.tag == "Player")
         {
             highlight.SetActive(false);
+            Close();
 
             if (interaction_debrief_UI != null)
             {
@@ -87,5 +115,18 @@ public class Interactables : MonoBehaviour
     public void PlayMiniGame()
     {
         //miniGame.SetActive(true); //if using scene minigame this should be a scene changer
+    }
+
+    public void SetUp(string prompt)
+    {
+        promptText.text = prompt;
+        uiPanel.SetActive(true);
+        isDisplayed = true;
+    }
+
+    public void Close()
+    {
+        uiPanel.SetActive(false);
+        isDisplayed = false;
     }
 }
