41b63342262b8006048617bb5540962da8eb819b
karizmaquiroz <quiroz.karizma@gmail.com>
2025-07-21 02:13:04 -0400

    Revert "Merge branch 'NewMain' into Karizma"
    This reverts commit ea32a8e5e825a31007a79e1ca7e9a7f919a1ab62, reversing
    changes made to fa93d708995030eb683c005a6907588b67d48ea2.

--- Paging Future Dr. Arrow/Assets/Scripts/Objects Scripts/Interactables.cs
+++ Paging Future Dr. Arrow/Assets/Scripts/Objects Scripts/Interactables.cs
@@ -1,11 +1,4 @@
-using TMPro;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.UI;
-using static System.Net.Mime.MediaTypeNames;
-using static UnityEditor.FilePathAttribute;
-using static UnityEngine.AdaptivePerformance.Provider.AdaptivePerformanceSubsystemDescriptor;
-using Text = UnityEngine.UI.Text;
 
 
 
@@ -14,120 +7,14 @@ using Text = UnityEngine.UI.Text;
 public class Interactables : MonoBehaviour
 {
 
-    public GameObject interaction_debrief_UI; //ref to Canvas or Text parent
-    private Text interaction_text; //ref to the Text element
-    private Text interaction_Debrief_Text;//ref to the text element (debrief text/notes on BMT)
+    [SerializeField] private ClipboardData noteData = null;
 
-    //npc dialogue interactions
-    [SerializeField] private string prompt; // prompt when interacting with object/person such as "read" or "speak"
-    [SerializeField] private GameObject uiPanel; // panel for prompt text
-    [SerializeField] private TextMeshPro promptText; // in game display of text string
-    
-    public Conversation convo;
-    public bool isDisplayed = false;
-
-
-    //[SerializeField] GameObject miniGame; //can be a physical 2d object or change to open a minigame scene
-
-    GameObject highlight; //object glows in range so player knows it is grabble/playable
-
-
-    void Start()
+    public void ShowNote()
     {
-        //get text component if interaction_Debrief_UI is the parent)
-        /*
-        if (interaction_debrief_UI != null)
-        {
-            interaction_text = interaction_Debrief_UI.GetComponentInChildren<Text>();
-            interaction_Debrief_UI.SetActive(false); //text will be hidden on start
-
-        }
-        */
-        uiPanel.SetActive(false);
-    }
-
-    private void FixedUpdate()
-    {
-        if (this.GetComponent<IsVisible>().IsObjectVisible() && isDisplayed && this.CompareTag("NPC")) {
-            //Debug.Log("Read");
-            if (Input.GetMouseButtonDown(0)) {
-                DialogueManager.StartConversation(convo);
-            }
-        }
+        ClipboardController.instance.CurrentNoteSource = ClipboardController.NoteSource.World;
+        ClipboardController.instance.ShowNote(noteData);
     }
 
 
-    private void OnEnable()
-    {
-        highlight = transform.GetChild(0).gameObject;
-    }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            highlight.SetActive(true);
-            SetUp();
-
-            if (interaction_debrief_UI != null)
-            {
-                interaction_debrief_UI.SetActive(true);//shows the text
-                //interaction_.Text = "Press [E} to read Me!";// set the text message
-
-            }
-
-            if (this.CompareTag("NPC")) {
-                //interaction_.Text = "Press [E} to read Me!";// set the text message
-
-            }
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            highlight.SetActive(false);
-            Close();
-
-            if (interaction_debrief_UI != null)
-            {
-                interaction_debrief_UI.SetActive(false);//hide the text
-    
-        }
-        }
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            interaction_Debrief_Text.gameObject.SetActive(true);
-
-        }
-
-    }
-
-    void DisplayPassage()
-    {
-        //set the debrief text content for the passage
-        interaction_Debrief_Text.text = "All Debrief text goes here.";
-    }
-
-    public void PlayMiniGame()
-    {
-        //miniGame.SetActive(true); //if using scene minigame this should be a scene changer
-    }
-
-    public void SetUp()
-    {
-        uiPanel.SetActive(true);
-        isDisplayed = true;
-    }
-
-    public void Close()
-    {
-        uiPanel.SetActive(false);
-        isDisplayed = false;
-    }
 }
