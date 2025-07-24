fd6f79b6e7112f80ae7e8e13aa2eadf28a8ea625
karizmaquiroz <quiroz.karizma@gmail.com>
2025-07-24 01:34:45 -0400

    MG3
    touch components should be working now

--- Paging Future Dr. Arrow/Assets/Scripts/MG3Scripts/MoveSystem.cs
+++ Paging Future Dr. Arrow/Assets/Scripts/MG3Scripts/MoveSystem.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 
 public class MoveSystem : MonoBehaviour
 {
-    public GameObject correctForm;
+    [SerializeField]
+    public Transform correctForm; //GameObject
+
+    private Vector2 initialPosition;
+
+
+
     private bool moving;
     private bool finish;
 
@@ -14,13 +20,16 @@ public class MoveSystem : MonoBehaviour
 
     private Vector3 resetPosition;
 
+
     private void Start()
     {
-        resetPosition = this.transform.localPosition;
+        //resetPosition = this.transform.localPosition;
+        initialPosition = transform.position;
     }
 
     private void Update()
     {
+        /*
         if (finish == false)
         {
 
@@ -33,42 +42,91 @@ public class MoveSystem : MonoBehaviour
                 this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, this.gameObject.transform.localPosition.z);
             }
         }
-    }
-    private void OnMouseDown()
-    {
-        if (Input.GetMouseButtonDown(0))
+        */
+        if (Input.touchCount > 0 && !finish)
         {
-            Vector3 mousePos;
-            mousePos = Input.mousePosition;
-            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
 
-            startPosX = mousePos.x - this.transform.localPosition.x;
-            startPosY = mousePos.y - this.transform.localPosition.y;
 
-            
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+                    {
+                        startPosX = touchPos.x - transform.position.x;
+                        startPosY = touchPos.y - transform.position.y;
 
-            moving = true;
+                    }
+                    break;
+                case TouchPhase.Moved:
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+                    {
+                        this.gameObject.transform.position = new Vector2(touchPos.x - startPosX, touchPos.y - startPosY);
+
+                    }
+                    break;
+                case TouchPhase.Ended:
+                    if (Mathf.Abs(transform.position.x - correctForm.transform.position.x) <= 0.5f &&
+                        Mathf.Abs(transform.position.y - correctForm.transform.position.y) <= 0.5f)
+                    {
+                        transform.position = new Vector2(correctForm.position.x, correctForm.position.y); // Vector 3 (...correctForm.transform.position.z);
+                        finish = true;
+
+                    }
+                    else
+                    {
+                        transform.position = new Vector2(initialPosition.x, initialPosition.y);
+                        // Vector 3 (...resetPosition.z);
+                        
+                    }
+                    break;
+
+            }
+
+
+            /*
+            private void OnMouseDown()
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Vector3 mousePos;
+                    mousePos = Input.mousePosition;
+                    mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+                    startPosX = mousePos.x - this.transform.localPosition.x;
+                    startPosY = mousePos.y - this.transform.localPosition.y;
+
+
+
+                    moving = true;
+
+                }
+            }
+            private void OnMouseUp() 
+            {
+                moving = false;
+
+                if (Mathf.Abs(this.transform.localPosition.x - correctForm.transform.localPosition.x) <= 0.5f && 
+                    Mathf.Abs(this.transform.localPosition.y - correctForm.transform.localPosition.y) <= 0.5f)
+                {
+                    this.transform.position = new Vector3(correctForm.transform.position.x, correctForm.transform.position.y, correctForm.transform .position.z);
+                    finish = true;
+
+
+                    //points system calls from scripts from other points
+                      GameObject.Find("PointHandler").GetComponent<WinScript>().AddPoints();
+                }
+                else
+                {
+                    this.transform.localPosition = new Vector3(resetPosition.x, resetPosition.y, resetPosition.z);
+                }
+            }
+
+
+        */
+
 
         }
     }
-    private void OnMouseUp() 
-    {
-        moving = false;
-
-        if (Mathf.Abs(this.transform.localPosition.x - correctForm.transform.localPosition.x) <= 0.5f && 
-            Mathf.Abs(this.transform.localPosition.y - correctForm.transform.localPosition.y) <= 0.5f)
-        {
-            this.transform.position = new Vector3(correctForm.transform.position.x, correctForm.transform.position.y, correctForm.transform .position.z);
-            finish = true;
-
-
-            //points system calls from scripts from other points
-              GameObject.Find("PointHandler").GetComponent<WinScript>().AddPoints();
-        }
-        else
-        {
-            this.transform.localPosition = new Vector3(resetPosition.x, resetPosition.y, resetPosition.z);
-        }
-    }
-
 }
